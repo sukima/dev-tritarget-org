2aeb6619d5a3081ab3eeeb21de153e57cdfd4059
Devin Weaver <suki@tritarget.org>
2016-03-25 08:31:15 -0400

    Add jsbin plugin

---
+++ plugins/jsbin/startup.js
@@ -0,0 +1,16 @@
+
+/*\
+title: $:/plugins/sukima/jsbin/startup.js
+type: application/javascript
+module-type: startup
+
+Embed a JS Bin
+
+\*/
+
+export function startup() {
+  if (!$tw.browser) { return; }
+  const jsbin = document.createElement('script');
+  jsbin.src = 'http://static.jsbin.com/js/embed.min.js?3.35.11';
+  document.getElementsByTagName('body')[0].appendChild(jsbin);
+}
