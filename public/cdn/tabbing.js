9a2699239962c099d33b20f365c45a8f683a184c
Devin Weaver <suki@tritarget.org>
2022-08-10 12:06:57 -0400

    Update focusable elements in tabbing.js module
    Prior to this change, I made my own list but found that others have
    [their own list][1] and is referenced by some blogs. I copied the same
    list.
    
    Also deduplicates the tab-able elements to prevent mutations making it
    into the WeakMap.
    
    Added a return value to the first tab-able element within the container
    so it is easy to .focus() if interested.
    
    [1]: https://github.com/gdkraus/accessible-modal-dialog/blob/master/modal-window.js#L38

--- public/cdn/tabbing.js
+++ public/cdn/tabbing.js
@@ -1,27 +1,38 @@
 const tabIndexes = new WeakMap();
 
-function* tabableElements(
+function* findTabableElements(
   selectAll = (selector) => document.querySelectorAll(selector)
 ) {
   yield* selectAll('a[href]');
-  yield* selectAll('button');
-  yield* selectAll('select');
-  yield* selectAll('textarea');
-  yield* selectAll('input:not([type=hidden])');
-  yield* selectAll('[contenteditable]');
+  yield* selectAll('area[href]');
+  yield* selectAll('input:not([disabled])');
+  yield* selectAll('select:not([disabled])');
+  yield* selectAll('textarea:not([disabled])');
+  yield* selectAll('button:not([disabled])');
+  yield* selectAll('iframe');
+  yield* selectAll('object');
+  yield* selectAll('embed');
+  yield* selectAll('*[tabindex]');
+  yield* selectAll('*[contenteditable]');
 }
 
 export function confineTabbing(container, selectAll) {
-  for (let element of tabableElements(selectAll)) {
+  let firstElementWithin = null;
+  let tabableElements = new Set(findTabableElements(selectAll));
+  for (let element of tabableElements) {
     tabIndexes.set(element, element.tabIndex);
-    if (!container.contains(element)) {
+    if (container.contains(element)) {
+      firstElementWithin ??= element;
+    } else {
       element.tabIndex = -1;
     }
   }
+  return firstElementWithin;
 }
 
 export function releaseTabbing(selectAll) {
-  for (let element of tabableElements(selectAll)) {
+  let tabableElements = new Set(findTabableElements(selectAll));
+  for (let element of tabableElements) {
     element.tabIndex = tabIndexes.get(element) || 0;
   }
 }
--- public/cdn/tests/tabbing-test.js
+++ public/cdn/tests/tabbing-test.js
@@ -7,24 +7,28 @@ module('tabbing.js', function(hooks) {
     this.fixture = document.querySelector('#qunit-fixture');
     this.fixture.innerHTML = `
       <div class="test-body">
-        <a data-orig="0" data-expected="0"></a>
-        <a href="#" data-orig="0" data-expected="-1"></a>
-        <button data-orig="0" data-expected="-1"></button>
-        <select data-orig="0" data-expected="-1"></select>
-        <textarea data-orig="0" data-expected="-1"></textarea>
-        <input type="hidden" data-orig="0" data-expected="0">
-        <input type="text" data-orig="0" data-expected="-1">
-        <input type="text" tabindex="2" data-orig="2" data-expected="-1">
+        <a id="test1" data-orig="0" data-expected="0"></a>
+        <a id="test2" href="#" data-orig="0" data-expected="-1"></a>
+        <button id="test3" data-orig="0" data-expected="-1"></button>
+        <select id="test4" data-orig="0" data-expected="-1"></select>
+        <textarea id="test5" data-orig="0" data-expected="-1"></textarea>
+        <input id="test6" type="hidden" data-orig="0" data-expected="-1">
+        <input id="test7" type="hidden" disabled data-orig="0" data-expected="0">
+        <input id="test8" type="text" data-orig="0" data-expected="-1">
+        <input id="test9" type="text" disabled data-orig="0" data-expected="0">
+        <input id="test10" type="text" tabindex="2" data-orig="2" data-expected="-1">
       </div>
       <dialog class="test-dialog">
-        <a data-orig="0" data-expected="0"></a>
-        <a href="#" data-orig="0" data-expected="0"></a>
-        <button data-orig="0" data-expected="0"></button>
-        <select data-orig="0" data-expected="0"></select>
-        <textarea data-orig="0" data-expected="0"></textarea>
-        <input type="hidden" data-orig="0" data-expected="0">
-        <input type="text" data-orig="0" data-expected="0">
-        <input type="text" tabindex="2" data-orig="2" data-expected="2">
+        <a id="test11" data-orig="0" data-expected="0"></a>
+        <a id="test12" href="#" data-orig="0" data-expected="0" data-first></a>
+        <button id="test13" data-orig="0" data-expected="0"></button>
+        <select id="test14" data-orig="0" data-expected="0"></select>
+        <textarea id="test15" data-orig="0" data-expected="0"></textarea>
+        <input id="test16" type="hidden" data-orig="0" data-expected="0">
+        <input id="test17" type="hidden" disabled data-orig="0" data-expected="0">
+        <input id="test18" type="text" data-orig="0" data-expected="0">
+        <input id="test19" type="text" disabled data-orig="0" data-expected="0">
+        <input id="test20" type="text" tabindex="2" data-orig="2" data-expected="2">
       </dialog>
     `;
     this.selectAll = s => this.fixture.querySelectorAll(s);
@@ -35,19 +39,24 @@ module('tabbing.js', function(hooks) {
   module('#confineTabbing', function() {
     test('confines tabindexes', function(assert) {
       const expectedTabIndex = e => parseInt(e.dataset.expected, 10);
-      confineTabbing(this.testDialog, this.selectAll);
+      let returnValue = confineTabbing(this.testDialog, this.selectAll);
+      assert.strictEqual(
+        returnValue,
+        this.fixture.querySelector('[data-first]'),
+        'returns first focusable element within'
+      );
       for (let element of this.selectAll('.test-body>*')) {
         assert.strictEqual(
           element.tabIndex,
           expectedTabIndex(element),
-          `fixture > ${element.nodeName}`
+          `fixture > ${element.nodeName} (${element.id})`
         );
       }
       for (let element of this.selectAll('.test-dialog>*')) {
         assert.strictEqual(
           element.tabIndex,
           expectedTabIndex(element),
-          `fixture > dialog > ${element.nodeName}`
+          `fixture > dialog > ${element.nodeName} (${element.id})`
         );
       }
     });
@@ -65,14 +74,14 @@ module('tabbing.js', function(hooks) {
         assert.strictEqual(
           element.tabIndex,
           expectedTabIndex(element),
-          `fixture > ${element.nodeName}`
+          `fixture > ${element.nodeName} (${element.id})`
         );
       }
       for (let element of this.selectAll('.test-dialog>*')) {
         assert.strictEqual(
           element.tabIndex,
           expectedTabIndex(element),
-          `fixture > dialog > ${element.nodeName}`
+          `fixture > dialog > ${element.nodeName} (${element.id})`
         );
       }
     });
